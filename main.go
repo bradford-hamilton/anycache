69e7efb4caa7acdf21f57d06c4c61c8744ffa5bd
bradford lamson-scribner <brad.lamson@gmail.com>
2023-11-30 15:44:50 -0700

    add incomparable struct example for blog post

--- main.go
+++ main.go
@@ -8,6 +8,25 @@ import (
 	"github.com/bradford-hamilton/anycache/pkg/anycache"
 )
 
+// incomparable is a zero-width, non-comparable type. Adding it to a struct
+// makes that struct also non-comparable, and generally doesn't add
+// any size (as long as it's first).
+//
+// From: https://github.com/shogo82148/go/blob/3839447ac39b1c49cb14833f0832e5f934e5bf6b/src/net/http/http.go#L22
+type incomparable [0]func()
+
+type incmpUser struct {
+	_ incomparable
+
+	name string
+	age  int
+}
+
+type cmpUser struct {
+	name string
+	age  int
+}
+
 func main() {
 	ac, err := anycache.New(10 * 1024 * 1024)
 	if err != nil {
@@ -18,5 +37,12 @@ func main() {
 		ac.Set(i, strconv.Itoa(i))
 	}
 
-	fmt.Println(ac.Keys())
+	ac.Set(cmpUser{name: "bradford", age: 34}, "valid")
+
+	item, ok := ac.Get(cmpUser{name: "bradford", age: 34})
+
+	fmt.Println(ok)
+	fmt.Println(item)
+
+	// ac.Set(incmpUser{name: "bradford", age: 34}, "panic")
 }
